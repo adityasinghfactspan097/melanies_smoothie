f6d963ad7bd2408ec079a158b1e1c11be0f58651
adityasinghfactspan097 <aditya.singh@factspan.com>
2024-10-27 20:07:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,4 +50,6 @@ if time_to_insert:
     st.success('your Smoothie is orderd!', icon='✅')
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+
+fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+
